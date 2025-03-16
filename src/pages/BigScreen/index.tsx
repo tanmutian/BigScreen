1eed756825a5290d596dffbdd4bc485b05e78a2a
tanmutian <tanmutian@126.com>
2025-03-16 12:54:06 +0000

    part3 50%

--- src/pages/BigScreen/index.tsx
+++ src/pages/BigScreen/index.tsx
@@ -8,9 +8,13 @@ import { useModel } from '@umijs/max';
 import TemplateComponentPrivate from './components/TemplateComponentPrivate';
 import TemplateComponentCommon from '@/components/TemplateComponentCommon';
 import styles from './index.less';
+import { Space, Table, Tag } from 'antd';
+import type { TableProps } from 'antd';
 import company_introduction from '@/assets/bigScreen/pic/company_introduction.png';
 import introduce2 from '@/assets/bigScreen/pic/introduce2.png'
 import user_number from '@/assets/bigScreen/pic/user_number.png'
+import device_stat from '@/assets/bigScreen/pic/device_status.png'
+//https://appstore.jiuxiniot.com/xy-3d-web/#/home
 
 export default () => {
   const {
@@ -26,6 +30,43 @@ export default () => {
     setComponentParamsPrivate
   } = useModel('BigScreen.model');
 
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      render: (text) => <a>{text}</a>,
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+      key: "age",
+    },
+  ];
+  
+  const data = [
+    {
+      key: "1",
+      name: "John Brown",
+      age: 32,
+      address: "New York No. 1 Lake Park",
+      tags: ["nice", "developer"],
+    },
+    {
+      key: "2",
+      name: "Jim Green",
+      age: 42,
+      address: "London No. 1 Lake Park",
+      tags: ["loser"],
+    },
+    {
+      key: "3",
+      name: "Joe Black",
+      age: 32,
+      address: "Sydney No. 1 Lake Park",
+      tags: ["cool", "teacher"],
+    },
+  ];
 
   return (
     <div className={styles.global}>
@@ -86,7 +127,42 @@ export default () => {
         </div>
 
         <div className={styles.device_status}>
-          设备状态
+          <div className={styles.company_title}>
+            <div className={styles.company_subtitle}>
+              设备状态
+            </div>
+          </div>
+          <div className={styles.date}>
+              <div className={styles.date_select}>
+                日
+              </div>
+              <div className={styles.date_specific}>
+                月
+              </div>
+              <div className={styles.date_specific}>
+                年
+              </div>
+          </div>
+
+          <div className={styles.device_img}>
+            <img src={device_stat}>
+            </img>
+          </div>
+
+          <div className={styles.device_text_small}>
+            设备总数
+          </div>
+          <div className={styles.device_text_large}>
+            0
+          </div>
+          <div className={styles.device_text_unit}>
+            台
+          </div>
+
+          <div className={styles.device_table}>
+            <Table columns={columns} dataSource={data} pagination={false}/>
+          </div>
+          
         </div>
       </div>
 
